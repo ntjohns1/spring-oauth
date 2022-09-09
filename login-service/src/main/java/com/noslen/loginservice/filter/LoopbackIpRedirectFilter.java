34f8920403a4b4aa3a56183822e5718236126fc8
ntjohns1 <noslen@Nelsons-Air.askey.com>
2022-09-09 02:00:55 -0400

    updates to endpoints for login service in auth/login servers

--- login-service/src/main/java/com/noslen/loginservice/filter/LoopbackIpRedirectFilter.java
+++ login-service/src/main/java/com/noslen/loginservice/filter/LoopbackIpRedirectFilter.java
@@ -15,24 +15,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * This filter ensures that the loopback IP <code>127.0.0.1</code> is used to access the
- * application so that the sample works correctly, due to the fact that redirect URIs with
- * "localhost" are rejected by the Spring Authorization Server, because the OAuth 2.1
- * draft specification states:
- *
- * <pre>
- *     While redirect URIs using localhost (i.e.,
- *     "http://localhost:{port}/{path}") function similarly to loopback IP
- *     redirects described in Section 10.3.3, the use of "localhost" is NOT
- *     RECOMMENDED.
- * </pre>
- *
- * @author Steve Riesenberg
- * @see <a href=
- * "https://tools.ietf.org/html/draft-ietf-oauth-v2-1-01#section-9.7.1">Loopback Redirect
- * Considerations in Native Apps</a>
- */
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class LoopbackIpRedirectFilter extends OncePerRequestFilter {
